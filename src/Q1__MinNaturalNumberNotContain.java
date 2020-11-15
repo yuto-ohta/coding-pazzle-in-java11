cca3bb750d38479a288abdcb7650873baffc5ad5
yuto-ohta <yutohta@gmail.com>
2020-11-15 12:34:01 +0900

    Mainクラスで実行確認するようにする

--- src/Q1__MinNaturalNumberNotContain.java
+++ src/Q1__MinNaturalNumberNotContain.java
@@ -5,16 +5,12 @@ import java.util.stream.Collectors;
 /*
 入力 マイナス、０を含む整数値の配列
 出力 配列に含まれていない最も小さい正の整数
+
 2020/11/14
  */
 
 class Q1__MinNaturalNumberNotContain {
-    public static void main(String[] args) {
-        int[] sample = {23, 45, 5, 35, 1, 2, 4, 5};
-        System.out.println(solution(sample));
-    }
-
-    public static int solution(int[] A) {
+    public int solution(int[] A) {
         List<Integer> arrayList = new ArrayList<>();
         for (int value : A) {
             arrayList.add(value);
